2c863414160ea1604fe09a37529d10a2c3d51741
Rishita2017 <96405597+Rishita2017@users.noreply.github.com>
2022-02-02 11:13:45 +0530

    Update dynamodb.py

--- dynamodb.py
+++ dynamodb.py
@@ -1,139 +1,98 @@
 import boto3
 from botocore.exceptions import ClientError
-client_dynamodb = boto3.client('dynamodb')
 
+client_dynamodb =boto3.client('dynamodb','ap-south-1')
 
-def book():
-    table = client_dynamodb.create_table(
-        TableName='TableOfTable',
-        KeySchema=[
-            {
-                'AttributeName': 'Category',
-                'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'Title',
-                'KeyType': 'RANGE'  # Sort key
-            }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'Category',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'Title',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'Language',
-                'AttributeType': 'S'
-            },
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
-    print("Creating Table")
-    return table
+class Book:
 
-
-def put_movie(category, title, language):
-    response = client_dynamodb.put_item(
-        TableName='TableOfBook',
-        Item={
-            'category': {
-                'N': "{}".format(category),
-            },
-            'title': {
-                'S': "{}".format(title),
-            },
-            'language': {
-                "S": "{}".format(language),
-            },
-
-        }
-    )
-    print("Loading data")
-    return response
-
-
-def get_movie(category, title):
-    try:
-        response = client_dynamodb.get_item(
-            TableName='TableOfBook',
-            Key={
-                'category': {
-                    'S': "{}".format(category),
+    def put_item(self, year, title, language, ):
+        response = client_dynamodb.put_item(
+            TableName='Books',
+            Item={
+                'year': {
+                    'N': year
+                    ,
                 },
                 'title': {
-                    'S': "{}".format(title),
-                }
+                    'S': title,
+                },
+                'language': {
+                    'S': language
+                },
+
+
+
             }
         )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        return response['Item']
+        print("Loading data")
+        return response
 
 
-def update_movie(category, title, rating, language, author):
-    response = client_dynamodb.update_item(
-        TableName='TableOfBook',
-        Key={
-            'category': {
-                'S': "{}".format(category),
+    def get_item( self, category, title):
+        try:
+            response = client_dynamodb.get_item(
+                TableName='Books',
+                Key={
+                    'category': {
+                        'S': category,
+                    },
+                    'title': {
+                        'S': title,
+                    }
+                }
+
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            return response['Item']
+
+
+    def update_book(self, category, title, language, rating):
+        response = client_dynamodb.update_item(
+
+            Key={
+                'category': {
+                    'S': category,
+                },
+                'title': {
+                    'S': title,
+                }
             },
-            'title': {
-                'S': "{}".format(title),
-            }
-        },
-        ExpressionAttributeNames={
-            '#R': 'rating',
-            '#P': 'language',
-            '#A': 'author'
-        },
-        ExpressionAttributeValues={
-            ':r': {
-                'N': "{}".format(rating),
+
+            UpdateExpression="set language=:l, rating=:r",
+            ExpressionAttributeValues={
+                ':l': language,
+                ':r': rating
             },
-            ':p': {
-                'S': "{}".format(language),
-            },
-            ':a': {
-                'SS': author,
-            }
-        },
-        UpdateExpression='SET #R = :r, #L = :l, #A = :a',
-        ReturnValues="UPDATED_NEW"
-    )
-    return response
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
 
 
 def delete_underrated_book(title, category, rating):
-    try:
-        response = client_dynamodb.delete_item(
-            TableName='TableOfBook',
-            Key={
-                'category': {
-                    'N': "{}".format(category),
+        try:
+            response = client_dynamodb.delete_item(
+                TableName='TableOfBooks',
+                Key={
+                    'category': {
+                        'N': "{}".format(category),
+                    },
+                    'title': {
+                        'S': "{}".format(title),
+                    }
                 },
-                'title': {
-                    'S': "{}".format(title),
+                ConditionExpression="rating <= :a",
+                ExpressionAttributeValues={
+                    ':a': {
+                        'N': "{}".format(rating),
+                    }
                 }
-            },
-            ConditionExpression="rating <= :a",
-            ExpressionAttributeValues={
-                ':a': {
-                    'N': "{}".format(rating),
-                }
-            }
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
-            print(e.response['Error']['Message'])
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+                print(e.response['Error']['Message'])
+            else:
+                raise
         else:
-            raise
-    else:
-        return response
+            return response
