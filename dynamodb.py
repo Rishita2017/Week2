227e19518bea42216b32421ac328af5c30a85ff2
Rishita2017 <96405597+Rishita2017@users.noreply.github.com>
2022-02-03 10:56:00 +0530

    Add files via upload

---
+++ dynamodb.py
@@ -0,0 +1,110 @@
+import boto3
+from botocore.exceptions import ClientError
+
+client_dynamodb = boto3.client('dynamodb', 'ap-south-1')
+
+
+class Book:
+
+    def put_item(self, category, title, language, year):
+        response = client_dynamodb.put_item(
+            TableName='TableOfBooks',
+            Item={
+                'category': {
+                    'S': category
+                    ,
+                },
+                'title': {
+                    'S': title,
+                },
+                'language': {
+                    'S': language,
+                },
+                'year': {
+                    'N': year,
+                }
+
+            }
+        )
+        print("Loading data")
+        return response
+
+
+    def get_item( self, category, title):
+        try:
+            response = client_dynamodb.get_item(
+                TableName='TableOfBooks',
+                Key={
+                    'category': {
+                        'S': category,
+                    },
+                    'title': {
+                        'S': title,
+                    }
+                }
+
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print(response['Item'])
+
+    def update_book(self,category, title, language, year, rating):
+        response = client_dynamodb.update_item(
+            TableName='TableOfBooks',
+            Key={
+                'category': {
+                    'S': category,
+                },
+                'title': {
+                    'S': title,
+                }
+            },
+            ExpressionAttributeNames={
+                '#L': 'language',
+                '#Y': 'year',
+                '#R': 'rating'
+            },
+            ExpressionAttributeValues={
+                ':l': {
+                    'S': language,
+                },
+                ':y': {
+                    'N': year,
+                },
+                ':r': {
+                    'N': rating,
+                }
+            },
+            UpdateExpression='SET #L = :l, #Y = :y, #R= :r',
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
+
+    def delete_underratedbook(self, category, title, rating):
+        try:
+            response = client_dynamodb.delete_item(
+                TableName='TableOfBooks',
+                Key={
+                    'category': {
+                        'S': category,
+                    },
+                    'title': {
+                        'S': title,
+                    }
+                },
+                ConditionExpression="rating <= :a",
+                ExpressionAttributeValues={
+                    ':a': {
+                        'N': rating,
+                    }
+                }
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+                print(e.response['Error']['Message'])
+            else:
+                raise
+        else:
+            return response
+
